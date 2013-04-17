e4d77c4999457a1ec427b33092515c10b058ac9a
Carlos Vásquez Polanco <carlosslim@gmail.com>
2013-04-17 02:25:19 -0430

    Agregada estructura inicial de widget

--- devdo-members/devdo-members.php
+++ devdo-members/devdo-members.php
@@ -6,4 +6,30 @@ Version: 0.1
 Author: Carlos V&aacute;squez Polanco
 Author URI: http://www.linkedin.com/in/carlosvasquez
 */
+
+function scripts() {
+    wp_enqueue_style(
+                'devdom_css',
+                path_join(WP_PLUGIN_URL, basename( dirname( __FILE__ ) ).'/devdomembers-styles.css'));
+
+  wp_enqueue_script(
+		'devdom_js',
+		path_join(WP_PLUGIN_URL, basename( dirname( __FILE__ ) ).'/devdomembers-script.js'));
+} 
+
+class devdomembers extends WP_Widget{
+    
+    function devdomembers(){
+        $widget_ops = array('classname' => 'devdomembers', 'description' => 'devdom' );
+        $this->WP_Widget('devdomembers', 'devdom', $widget_ops);
+    }
+
+    function widget($args, $instance) {
+        extract($args, EXTR_SKIP);
+ 
+     }
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("devdomembers");'));
+add_action('wp_enqueue_scripts', 'scripts');
 ?>
