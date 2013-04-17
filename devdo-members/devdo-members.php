794acfa6dcd73ecc3f5d4e72831a34f1a7bf6352
Carlos Vásquez Polanco <carlosslim@gmail.com>
2013-04-16 23:34:51 -0430

    Inicialización de plugin para developers.do

---
+++ devdo-members/devdo-members.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: devdo-members
+Description: Plugin de los miembros de la comunidad de developers Dominicanos
+Version: 0.1
+Author: Carlos V&aacute;squez Polanco
+Author URI: http://www.linkedin.com/in/carlosvasquez
+*/
+?>
